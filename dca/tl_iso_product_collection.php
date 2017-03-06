daa3ac57cbb2a7a42131e96467b71d94c27810de
fbliss <fred.bliss@intelligentspark.com>
2017-03-06 16:13:46 -0500

    fixed psr-0 path

--- dca/tl_iso_product_collection.php
+++ dca/tl_iso_product_collection.php
@@ -1,10 +1,10 @@
-<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
-
+<?php
 /**
  * @copyright  Intelligent Spark 2017
  * @author     Fred Bliss <fred.bliss@intelligentspark.com>
  * @license    http://opensource.org/licenses/lgpl-3.0.html
  */
+
 
 
 /**
